a652e6828faf8d62389da34989d92fe29150a695
Solomon1d24 <solo5698@connect.hku.hk>
2023-03-16 01:18:44 +0800

    Change the sample

--- Music Advisor/task/src/HttpSample.java
+++ Music Advisor/task/src/HttpSample.java
@@ -11,38 +11,4 @@ import java.net.http.HttpResponse;
 
 public class HttpSample {
 
-    public static void main(String[] args) {
-
-        try {
-            HttpServer httpServer = HttpServer.create();
-            httpServer.bind(new InetSocketAddress(8080), 0);
-            httpServer.createContext("/", new HttpHandler() {
-                @Override
-                public void handle(HttpExchange exchange) throws IOException {
-                    String content = "Hello World";
-                    exchange.sendResponseHeaders(200, content.length());
-                    exchange.getResponseBody().write(content.getBytes());
-                    exchange.getResponseBody().close();
-                }
-            });
-
-            httpServer.start();
-            try {
-                HttpClient httpClient = HttpClient.newBuilder().build();
-                HttpRequest httpRequest = HttpRequest.newBuilder()
-                        .uri(URI.create("http://localhost:8080"))
-                        .GET()
-                        .build();
-                HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-                System.out.println(httpResponse.body());
-            } catch (IOException | InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            httpServer.stop(1);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
